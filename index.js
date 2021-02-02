6050a8bc9960be031a451126fb1790f5b64c1234
Your NameHamilton <hamilton@hts-net.com.br>
2021-02-01 21:58:16 -0300

    segunda rota

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
  
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello World, it´s me!')
 })
- 
+
+app.get('/mensagens', function(request, response) {
+    response.send('Exibir todas as mensagens!')
+})
+
 app.listen(3000)
